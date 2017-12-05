bae272bf74892e709b8512428c138e7ad94b097d
samelie <sam@stinkdigital.com>
2017-12-04 17:34:34 -0800

    delete old code

--- src/js/common/mouse.js
+++ src/js/common/mouse.js
@@ -59,94 +59,10 @@ function Mouse(container, regl) {
     }
     _previousX = e.pageX
     _previousY = e.pageY
-
-    /*translation[0] = (e.pageX / window.innerWidth - 0.5) * 2
-    translation[1] =
-      ((window.innerHeight - e.pageY) / window.innerHeight - 0.5) * 2
-    translation[2] = -5
-
-    modelMatrix[12] = (e.pageX / window.innerWidth - 0.5) * 2
-    modelMatrix[13] =
-      ((window.innerHeight - e.pageY) / window.innerHeight - 0.5) * 2
-    modelMatrix[14] = 0.1*/
   })
 
   const isStill = () => _isStill
   const getClicks = () => _clickCounter * 0.5
-
-  const icosphere = require("primitive-icosphere")(1, {
-    subdivisions: 1,
-  })
-
-  /*const modelMatrix = mat4.create()
-  mat4.translate(modelMatrix, modelMatrix, [0.5, 0.5, -10])
-  const COLOR = [1, 0.1, 0.1]
-  const draw = () => {
-    if (_canDraw) {
-      regl({
-        vert: `
-    precision lowp float;
-    uniform mat4 projection, view, model;
-    attribute vec3 position;
-    attribute vec3 normal;
-    uniform vec3 color;
-    uniform float scale;
-
-    varying vec3 vColor;
-
-          mat3 rotateY(float rad) {
-          float c = cos(rad);
-          float s = sin(rad);
-          return mat3(
-              c, 0.0, -s,
-              0.0, 1.0, 0.0,
-              s, 0.0, c
-          );
-      }
-
-
-    void main () {
-      vec3 pos = position * scale;
-      vec3 norm = normal;
-      float cosTheta = dot(normal, vec3(0.39, 0.87, 0.29));
-      vColor = norm / 2. + clamp(color * cosTheta,0.,1.);
-      gl_Position = projection * view * model * vec4(pos, 1);
-    }
-    `,
-        frag: `
-    precision lowp float;
-    varying vec3 vColor;
-
-    void main () {
-      gl_FragColor = vec4(vColor, 1.);
-    }
-    `,
-
-        uniforms: {
-          model: modelMatrix,
-          scale: regl.prop("scale"),
-          view: regl.context("view"),
-          projection: regl.context("projection"),
-          color: COLOR,
-        },
-
-        attributes: {
-          position: icosphere.positions,
-          normal: icosphere.normals,
-        },
-
-        elements: icosphere.cells,
-      })({scale:1})
-    }
-  }
-
-  const updateMeshPosition = pos=>{
-    modelMatrix[12] = pos[0]
-    modelMatrix[13] =pos[1]
-    modelMatrix[14] =pos[2]
-    console.log(pos);
-    _canDraw = true
-  }*/
 
   return {
     //updateMeshPosition,
